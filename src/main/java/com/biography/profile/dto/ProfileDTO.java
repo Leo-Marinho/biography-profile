2934f2e63238217078ed8807f1c5c629af1707cb
Leo-Marinho <leomarinho.is@icloud.com>
2020-04-10 21:30:09 -0300

    implements methods

---
+++ src/main/java/com/biography/profile/dto/ProfileDTO.java
@@ -0,0 +1,28 @@
+package com.biography.profile.dto;
+
+import com.biography.profile.model.ProfileEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProfileDTO {
+
+    private String name;
+
+    private Integer age;
+
+    private String biography;
+
+    private String email;
+
+    private String contact;
+
+    public ProfileDTO(final ProfileEntity profileEntity){
+        this.name = profileEntity.getName();
+        this.age = profileEntity.getAge();
+        this.biography = profileEntity.getBiography();
+        this.email = profileEntity.getEmail();
+        this.contact = profileEntity.getContact();
+    }
+}
